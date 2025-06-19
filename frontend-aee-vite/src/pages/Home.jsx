9691b2fbf5fc3e769be1846dc9ea62d7fa72ccde
Gerson Santiago <gersondossantossantiago@gmail.com>
2025-06-19 17:59:46 -0300

    feat: finaliza estilização e responsividade da Home

--- frontend-aee-vite/src/pages/Home.jsx
+++ frontend-aee-vite/src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
+import { useNavigate } from 'react-router-dom'
+import './Home.css'
 
 export default function Home() {
-  const navigate = useNavigate();
-
+  const navigate = useNavigate()
   return (
     <div className="home-container">
-      <h1>Bem-vindo ao Sistema</h1>
-      <p>Essa é a página pública do sistema. Acesse o login para continuar.</p>
-      <button onClick={() => navigate('/login')}>Ir para o Login</button>
+      <div className="home-card">
+        <h1>Bem-vindo ao Sistema</h1>
+        <p>Para acessar os recursos do sistema, utilize sua conta institucional. Clique abaixo para fazer login.</p>
+        <button onClick={() => navigate('/login')}>
+          Ir para o Login <span className="material-symbols-rounded">login</span>
+        </button>
+      </div>
     </div>
-  );
+  )
+
 }
